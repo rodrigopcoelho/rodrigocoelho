c45267b001dd302556e6c4590205860af0a33057
Rodrigo Coelho <rodrigopcoelho2000@gmail.com>
2023-09-05 09:17:41 +0100

    arranjado

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,57 +1,61 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function About() {
   const navigate = useNavigate();
 
   const projects = () => {
-      navigate("/projects");
-  }
+    navigate("/projects");
+  };
   const about = () => {
-    navigate("/about");
-  }
+    navigate("#");
+  };
   return (
-    <div className='backblue'>
-      <div className='d-flex justify-content-between py-2 px-3 text-white'> <h1 className="red" onClick={about}>ABOUT ME</h1> <h1  onClick={projects}>PROJECTS</h1> </div>
-      <div className='d-flex align-items-center '>
 
-      <div className="full-height-container d-flex flex-column justify-content-around px-3">
-        <h1 className='text-start'>ABOUT</h1>
-        <h1 className='text-start'>SKILLS</h1>
-        <h1 className='text-start'>EDUCATION</h1>
+    <div className="backblue">
+      <div className="d-flex justify-content-between py-2 px-3 text-white">
+
+        <h1  className="red aA" onClick={about} style={{cursor: "pointer"}}>
+          ABOUT ME
+        </h1>
+        <h1 className="pA" onClick={projects} style={{cursor: "pointer"}}>PROJECTS</h1>
       </div>
+      <div className="d-flex align-items-center ">
+        <div className="full-height-container d-flex flex-column justify-content-around px-3">
+          <h1 className="text-start">ABOUT</h1>
+          <h1 className="text-start">SKILLS</h1>
+          <h1 className="text-start  mb-5">EDUCATION</h1>
+        </div>
 
-      <div className='divabout '>
-        <h3>ABOUT</h3>
-        <p>Full-Stack Developer with extensive experience as a front-end and web designer, with a degree in Technology and Communication.
-
-I have experience in various projects. The ability to quickly learn and apply new technologies is one of my strongest assets.
-
-A degree and an intensive bootcamp taught me the importance of priorities and teamwork. I am a diligent, creative, and curious professional who always enjoys learning more. I have a positive approach and enjoy challenges that allow me to enhance my skills.</p>
-        <h3>SKILLS</h3>
-        <p>Programming Languages
-Ruby, JavaScript, HTML, CSS, Python, C#, kotlin
-
-Frameworks/Libraries
-React, Rails, Bootstrap, Ajax, Jetpack Compose
-
-Databases/ORM
-SQL, FireBase, Neo4j, SQLite, Postgresql, Node.js
-
-other
-Git, GitHub, Heroku, REST API, Figma, Blender, Unity3d</p>
-        <h3>EDUCATION</h3>
-        <p>Le Wagon</p>
-        <p>Full Stack Web Development</p>
-        <p></p>
-        <p>Aveiro University </p>
-        <p>Degree in Multimedia and Communication Technologies</p>
+        <div className="divabout pb-5">
+          <h3>ABOUT</h3>
+          <p>
+            Full-Stack Developer with extensive experience as a front-end and
+            web designer, with a degree in Technology and Communication. <br/><br/>I have
+            experience in various projects. The ability to quickly learn and
+            apply new technologies is one of my strongest assets.<br/><br/> A degree and
+            an intensive bootcamp taught me the importance of priorities and
+            teamwork. I am a diligent, creative, and curious professional who
+            always enjoys learning more. I have a positive approach and enjoy
+            challenges that allow me to enhance my skills.<br/><br/>
+          </p>
+          <h3>SKILLS</h3>
+          <p>
+            <span className="red">Programming Languages</span><br/> Ruby, JavaScript, HTML, CSS, Python, C#,
+            kotlin<br/><br/> <span className="red">Frameworks/Libraries</span><br/> React, Rails, Bootstrap, Ajax, Jetpack
+            Compose<br/><br/> <span className="red">Databases/ORM</span><br/> SQL, FireBase, Neo4j, SQLite, Postgresql,
+            Node.js<br/><br/> <span className="red">Other</span><br/> Git, GitHub, Heroku, REST API, Figma, Blender, Unity3d
+            <br/><br/></p>
+          <h3>EDUCATION</h3>
+          <p>Le Wagon</p>
+          <p>Full Stack Web Development<br/><br/></p>
+          <p></p>
+          <p>Aveiro University </p>
+          <p className="pb-4">Degree in Multimedia and Communication Technologies</p>
+        </div>
       </div>
-
-      </div>
-
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,6 @@ import banner from '../assets/svg/banner.svg';
 import polaroid from '../assets/img/polaroid.png';
 import { useNavigate } from "react-router-dom";
 
-
 function Home() {
   const [loading, setLoading] = useState(false);
 
@@ -18,6 +17,7 @@ function Home() {
   useEffect (() => {
     setLoading (true)
     setTimeout (() => {
+      setLoadingHeight(prevHeight => prevHeight + 70);
     setLoading(false)
   }, 5000)}, []);
 
@@ -45,12 +45,13 @@ function Home() {
 
         :
         <>
+
         <div className='d-flex left-side align-items-end '>
         <img src={banner} alt="banner" className='banner'/>
-
         </div>
+
         <div className='right-side'>
-          <div className='d-flex justify-content-between py-2 px-3'> <h1 onClick={about}>ABOUT ME</h1> <h1 onClick={projects}>PROJECTS</h1> </div>
+          <div className='d-flex justify-content-between py-2 px-3'> <h1 onClick={about} style={{cursor: "pointer"}} >ABOUT ME</h1> <h1 onClick={projects} style={{cursor: "pointer"}}>PROJECTS</h1> </div>
           <div className='d-flex justify-content-end align-items-center pe-3 pt-3'><p>Born in 2000,<br/>in Lisbon, Portugal.<br/>I believe <br/>web development<br/>can be more<br/>diverse and empowering.<br/>With a mission<br/>to explore<br/>the endless possibilities<br/> of web design,<br/>I am dedicated<br/>to pushing boundaries<br/> and creating innovative solutions.<br/>Through constant experimentation<br/>and a passion for coding,<br/>I aim to redefinethe digital landscape<br/>and deliver unique experiences.</p></div>
           <img src={polaroid} alt="polaroid" className='polaroid'/>
         </div>
